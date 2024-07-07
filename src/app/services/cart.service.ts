2346594b0e2db03bb8abf2fc8c9615427fc59852
MarkoMilenovic01 <markomilenovic7@gmail.com>
2024-07-07 20:05:23 +0200

    Handling Browser Refresh

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,8 +12,20 @@ export class CartService {
   totalPrice : Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity : Subject<number> = new BehaviorSubject<number>(0);
 
+  storage : Storage = sessionStorage;
 
-  constructor() { }
+  constructor() {
+
+      // read data from storage
+      let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+      if(data != null){
+        this.cartItems = data;
+
+        this.computeCartTotals();
+      }
+
+   }
 
 
   addToCart(theCartItem: CartItem){
@@ -57,6 +69,12 @@ export class CartService {
 
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
+
+    this.persistCartItems();
+  }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
 
